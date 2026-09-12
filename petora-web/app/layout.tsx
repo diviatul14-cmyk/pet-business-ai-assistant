5a5628cdf52f4cff019f41ec03fa35df1e242b95
Divyanshu Lal <divyanshulal@Divyanshus-MacBook-Air.local>
2026-09-12 19:42:10 +0530

    Add Google Analytics tracking

--- petora-web/app/layout.tsx
+++ petora-web/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -66,7 +67,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-2B07YW3NWJ"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-2B07YW3NWJ');
+          `}
+        </Script>
+      </body>
     </html>
   );
 }
